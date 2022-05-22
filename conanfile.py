6e167b7be2049ddde8176724d0cd861a10ba5287
Heiko Lewin <hlewin@worldiety.de>
2022-05-22 11:51:01 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -11,7 +11,7 @@ class HarfbuzzConan(ConanFile):
     
     description = "HarfBuzz is an OpenType text shaping engine."
     topics = ("opentype", "text", "engine")
-    url = "https://github.com/conan-io/conan-center-index"
+    url = "https://gitlab.worldiety.net/worldiety/customer/wdy/libriety/cpp/forks"
     homepage = "http://harfbuzz.org"
     license = "MIT"
 
@@ -37,9 +37,10 @@ class HarfbuzzConan(ConanFile):
 
     short_paths = True
 
-    exports_sources = "*"
-    generators = "cmake_paths"
-    no_copy_source=True
+    no_copy_source = True
+    exports_sources = ["*"]
+    python_requires = "wdyConanHelper/[]"
+    python_requires_extend = "wdyConanHelper.ConanCMake"
 
 
     def config_options(self):
@@ -54,51 +55,27 @@ class HarfbuzzConan(ConanFile):
         if self.options.with_freetype:
             self.requires("freetype/[]")
 
-    @functools.lru_cache(1)
-    def _configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["HB_HAVE_FREETYPE"] = self.options.with_freetype
-        cmake.definitions["HB_HAVE_GRAPHITE2"] = False
-        cmake.definitions["HB_HAVE_GLIB"] = False
-        cmake.definitions["HB_HAVE_ICU"] = False
-        if tools.is_apple_os(self.settings.os):
-            cmake.definitions["HB_HAVE_CORETEXT"] = True
-        elif self.settings.os == "Windows":
-            cmake.definitions["HB_HAVE_GDI"] = self.options.with_gdi
-            cmake.definitions["HB_HAVE_UNISCRIBE"] = self.options.with_uniscribe
-            cmake.definitions["HB_HAVE_DIRECTWRITE"] = self.options.with_directwrite
-        cmake.definitions["HB_BUILD_UTILS"] = False
-        cmake.definitions["HB_BUILD_SUBSET"] = self.options.with_subset
-        cmake.definitions["HB_HAVE_GOBJECT"] = False
-        cmake.definitions["HB_HAVE_INTROSPECTION"] = False
-        cmake.definitions["CMAKE_PROJECT_" + self.name + "_INCLUDE"] = os.path.join(self.build_folder, "conan_paths.cmake")
+    def cmake_definitions(self):
+        defs = {
+            "HB_HAVE_FREETYPE": self.options.with_freetype,
+            "HB_HAVE_GRAPHITE2": False,
+            "HB_HAVE_GLIB": False,
+            "HB_HAVE_ICU": False,
+            "HB_BUILD_UTILS": False,
+            "HB_BUILD_SUBSET": self.options.with_subset,
+            "HB_HAVE_GOBJECT": False,
+            "HB_HAVE_INTROSPECTION": False,
+        }
 
-        debug_prefix_mapping = '-ffile-prefix-map=' + os.path.abspath(self.source_folder) + '=' + self.name
-        cmake.definitions["CMAKE_C_FLAGS"] = debug_prefix_mapping
-        cmake.definitions["CMAKE_CXX_FLAGS"] = debug_prefix_mapping
-        
-        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.get_safe("fPIC", True)
-        cmake.definitions["CMAKE_INSTALL_RPATH_USE_LINK_PATH"] = True
-        
-        debug_prefix_mapping = '-ffile-prefix-map=' + os.path.abspath(self.source_folder) + '=' + self.name
-        cmake.definitions["CMAKE_C_FLAGS"] = debug_prefix_mapping
-        cmake.definitions["CMAKE_CXX_FLAGS"] = debug_prefix_mapping
-                
+        if tools.is_apple_os(self.settings.os):
+            defs["HB_HAVE_CORETEXT"] = True
+        elif self.settings.os == "Windows":
+            defs["HB_HAVE_GDI"] = self.options.with_gdi
+            defs["HB_HAVE_UNISCRIBE"] = self.options.with_uniscribe
+            defs["HB_HAVE_DIRECTWRITE"] = self.options.with_directwrite
         # fix for MinGW debug build
         if self.settings.compiler == "gcc" and self.settings.os == "Windows":
-            cmake.definitions["CMAKE_C_FLAGS"] = "-Wa,-mbig-obj " + debug_prefix_mapping
-            cmake.definitions["CMAKE_CXX_FLAGS"] = "-Wa,-mbig-obj " + debug_prefix_mapping
-
-        cmake.configure(source_folder=self.source_folder)
-        return cmake
-
-    def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
-
-    def package(self):
-        self.copy("COPYING", dst="licenses", src=self.source_folder)
-        cmake = self._configure_cmake()
-        cmake.install()
-
+            defs["CMAKE_C_FLAGS"] = "-Wa,-mbig-obj " + debug_prefix_mapping
+            defs["CMAKE_CXX_FLAGS"] = "-Wa,-mbig-obj " + debug_prefix_mapping
+        return defs
 
