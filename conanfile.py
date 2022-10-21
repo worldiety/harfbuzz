e6dc5fba052c7f84fbaf7b31684f31989cbef5b8
Heiko Lewin <heiko.lewin@worldiety.de>
2022-10-21 14:48:36 +0200

    Update to 5.3.1

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ required_conan_version = ">=1.33.0"
 
 class HarfbuzzConan(ConanFile):
     name = "harfbuzz"
-    version = "5.3.0"
+    version = "5.3.1"
     
     description = "HarfBuzz is an OpenType text shaping engine."
     topics = ("opentype", "text", "engine")
